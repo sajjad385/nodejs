ab8d4313c90c18c4c364dd9f0671a7c6efa0f798
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2021-07-21 00:52:06 +0600

    synchronous File system

--- fsModule/index.js
+++ fsModule/index.js
@@ -3,12 +3,18 @@ var fs = require('fs')
 
 var server = http.createServer(function (request, response) {
     //Asynchronous File system
-    fs.readFile('home.html', function (error, data) {
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        response.write(data)
-        response.end()
+    // fs.readFile('home.html', function (error, data) {
+    //     response.writeHead(200, {'Content-Type': 'text/html'})
+    //     response.write(data)
+    //     response.end()
+    //
+    // })
+    //synchronous File system
+    var data = fs.readFileSync('home.html')
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.write(data)
+    response.end()
 
-    })
 });
 server.listen(5050)
 console.log('Complied Successfully')
