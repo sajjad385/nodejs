3898c11426432a8f8354125d0a5f867680241503
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2021-07-21 00:45:53 +0600

    File system module

---
+++ fsModule/index.js
@@ -0,0 +1,14 @@
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (request, response) {
+    //Asynchronous File system
+    fs.readFile('home.html', function (error, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.write(data)
+        response.end()
+
+    })
+});
+server.listen(5050)
+console.log('Complied Successfully')
