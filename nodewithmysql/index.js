fd17519a9a4fbb7b47c0d9b6553d25f249d48b95
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2021-07-22 02:04:57 +0600

    connect with database

--- nodewithmysql/index.js
+++ nodewithmysql/index.js
@@ -2,13 +2,10 @@ let mysql = require('mysql')
 let databaseConnectionConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'password'
+    password: 'password',
+    database:'nodejs'
 }
 let con = mysql.createConnection(databaseConnectionConfig)
 con.connect(function (error) {
-    if (error) {
-        console.log('Database Connection Failed')
-    } else {
-        console.log('Database Connection Successfully!')
-    }
+    error ? console.log('Database Connection Failed'): console.log('Database Connection Successfully!')
 })
